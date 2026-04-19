d95eecd4c7d6078f9fe95e43a63add4bf8da4a8d
najcoding <enoughnaj@gmail.com>
2026-04-19 15:00:29 +0100

    Add files via upload

---
+++ Friut Quiz.py
@@ -0,0 +1,22 @@
+class FruitQuiz:
+    def __init__(self):
+        def __init__(self):
+            self.fruits = {'apple':'red',
+                           'orange':'orange',
+                           'watermelon':'green',
+                           'banana':'yellow'}
+            def quiz(self):
+                while (True):
+                    fruit, colour = random.choice(list(self.fruits.items()))
+                    print("What is the colour of{}".format(fruit))
+                    user_answer = input()
+                    if(user_answer.lower() == colour):
+                        print("Correct answer")
+                    else:
+                        print("Wrong answer")
+                    option = int(input("enter 0, if you want to play again otherwise enter 1:"))
+                    if (option):
+                        break
+print("Welcome to fruit quiz")
+fq = FruitQuiz()
+fq.quiz
